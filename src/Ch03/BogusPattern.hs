5ae84e33ec60c9be302d2bb9b185be584db961f2
chenyulue <chenyulue@163.com>
2022-08-22 22:45:23 +0800

    03-Conditional evaluation with guards at 2022年08月22日 22:45:23

--- src/Ch03/BogusPattern.hs
+++ src/Ch03/BogusPattern.hs
@@ -1,6 +1,8 @@
 module Ch03.BogusPattern where
 
 -- File: src/Ch03/BogusPattern.hs
+data Fruit = Apple | Orange
+
 betterFruit f = case f of
                   "apple"  -> Apple
                   "orange" -> Orange
